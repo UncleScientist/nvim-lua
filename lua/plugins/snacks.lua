07580541cedcef32f2dd50099da33e8388c9c6f0
Uncle Scientist <unclescientist@gmail.com>
2025-12-25 09:01:07 -0500

    trying out the snacks plugin

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,28 @@
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        bigfile = { enabled = true },
+        dashboard = { enabled = true },
+        explorer = { enabled = true },
+        indent = { enabled = true },
+        input = { enabled = true },
+        picker = { enabled = true, sources = { explorer = { focus = "input", auto_close = true } } },
+        notifier = { enabled = true },
+        quickfile = { enabled = true },
+        scope = { enabled = true },
+        scroll = { enabled = true },
+        statuscolumn = { enabled = false },
+        words = { enabled = false },
+    },
+    keys = {
+        {
+            "<space>o",
+            function()
+                Snacks.explorer()
+            end,
+            desc = "File explorer",
+        },
+    },
+}
